d06dd4acb64c9253311b81ccdc95f57bad45b668
lauri koti <lauriaus@metropoli.fi>
2020-03-17 10:29:18 +0200

    'week1'

---
+++ week1/server-one/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/catinfo', (req, res) => {
+    const cat = {
+        'name': 'Frank',
+        'age': 6,
+        'weight': 5,
+    };
+    res.json(cat);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ week1/server-two/app.js
@@ -0,0 +1,18 @@
+
+const pug = require('pug');
+
+// Compile the source code
+const compiledFunction = pug.compileFile('template.pug');
+
+// Render a set of data
+console.log(compiledFunction({
+    name: 'Timothy'
+}));
+// "<p>Timothy's Pug source code!</p>"
+
+// Render another set of data
+console.log(compiledFunction({
+    name: 'Forbes'
+}));
+
+// Compile template.pug, and render a set of data
