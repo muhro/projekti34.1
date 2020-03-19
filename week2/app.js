4041caf2233330072eb9598d3f9a077503a7ab57
lauri koti <lauriaus@metropoli.fi>
2020-03-19 09:11:38 +0200

    'toimii'

--- week2/app.js
+++ week2/app.js
@@ -3,8 +3,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/cat', (req, res) => {
+app.get('/cat', (req, res, next) => {
   res.send('From this endpoint you can get cats.')
+  next()
+});
+app.post('/cat', (req, res) => {
+  res.send('With this endpoint you can add cats.')
+});
+app.delete('/cat', (req, res) =>{
+  res.send('With this endpoint you can delete cats.')
+});
+app.put('/cat', (req, res) =>{
+  res.send('With this endpoint you can edit cats.')
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
