39d881ef4daf62883349b1503cb4bade7717a198
lauri koti <lauriaus@metropoli.fi>
2020-03-20 11:43:52 +0200

    'midle'

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -4,16 +4,16 @@ const express = require('express');
 const router = express.Router();
 const catController = require('../controllers/catController');
 const multer  = require('multer');
-const upload = multer({ dest: '../uploads/' });
+const upload = multer({ dest: './uploads/' });
 
 const app = express();
 
-app.post('/', upload.single('avatar'), (req, res, next) =>{
+router.post('/', upload.single('avatar'), (req, res, next) =>{
 });
-app.post('/', upload.array('cat', 12), function (req, res, next) {
+router.post('/', upload.array('cat', 12), function (req, res, next) {
 });
 const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cat', maxCount: 8 }])
-app.post('/', cpUpload, function (req, res, next) {
+router.post('/', cpUpload, function (req, res, next) {
 });
 
 router.get('/', catController.cat_list_get);
@@ -33,10 +33,7 @@ router.get('/:id', (req, res) => {
     res.send(id)
 });
 
-router.post('/', (req, res) => {
-    res.send('With this endpoint you can add cats.')
-    console.log(req.body)
-});
+
 
 router.delete('/', (req, res) =>{
     res.send('With this endpoint you can delete cats.')
