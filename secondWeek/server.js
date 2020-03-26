f95dcf26e04db6dc7eea4e1497ebf0f827589468
lauri koti <lauriaus@metropoli.fi>
2020-03-26 10:08:45 +0200

    pikkujuttui

--- secondWeek/server.js
+++ secondWeek/server.js
@@ -1,5 +1,4 @@
 'use strict';
-
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -9,19 +8,19 @@ const user = require('./model/user');
 app.use(express.urlencoded({extended: false}));
 app.use('/cat', require('./route'));
 
-
 app.get('/',async (req, res)=>{
     res.send(await cat.find());
 });
 app.post('/cat', async(req, res) => {
-    const mycat = await cat.create({name: '', age: '', owner: '', gender: '', color: '', weight: ''});
-    res.send('cat creted with id: ${mycat._id}')
+    const myCat = await cat.create({name: '', age: '', owner: '', gender: '', color: '', weight: ''});
+    const text = 'cat created with id:';
+    res.send(text, myCat._id)
 });
 app.post('/user', async(req, res) => {
-    const myuser = await user.create({name: 'Mary', email: 'm@gmail.com', password: 'abc'});
-    res.send('user creted with id: ${myuser._id}');
+    const myUser = await user.create({name: 'Mary', email: 'm@gmail.com', password: 'abc'});
+    const text = 'user created with id:';
+    res.send(text, myUser._id);
 });
-
 db.on('connected', () => {
     app.listen(3000);
 });
